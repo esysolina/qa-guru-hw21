cf652dfdb801cecb6ffbd07b205777be606e656b
esysolina <evelina.ova@gmail.com>
2022-04-19 22:57:06 +0500

    initial commit

---
+++ src/test/java/guru/qa/tests/AndroidSelenideHomeWorkTests.java
@@ -0,0 +1,28 @@
+package guru.qa.tests;
+
+import io.appium.java_client.MobileBy;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+
+public class AndroidSelenideHomeWorkTests extends TestBase {
+    @Owner("esysolina")
+    @DisplayName("Search in wiki")
+    @Test
+    void searchTest() {
+        step("Pass welcome window", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click());
+        step("Search input", () -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        });
+        step("Verify content found", () ->
+                $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0)));
+    }
+}
